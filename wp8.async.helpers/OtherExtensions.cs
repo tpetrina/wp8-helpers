9d7a8f7679408989dc96c852309a39f298bae273
Toni Petrina <toni.petrina@gmail.com>
2013-02-12 20:43:44 +0100

    Added one more async extension for Storyboard.

---
+++ wp8.async.helpers/OtherExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace WP8.Async.Helpers
+{
+    public static class OtherExtensions
+    {
+        /// <summary>
+        /// Initiates the set of animations associated with the storyboard asynchronously.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns>A task that will complete when the storyboard is finished.</returns>
+        public static Task BeginAsync(this Storyboard @this)
+        {
+            if (@this == null)
+                throw new NullReferenceException();
+
+            // it doesn't really matter which type is used here.
+            var tcs = new TaskCompletionSource<bool>();
+            EventHandler handler = null;
+            handler = (sender, e) =>
+                {
+                    @this.Completed -= handler;
+                    tcs.TrySetResult(true);
+                };
+            @this.Completed += handler;
+            @this.Begin();
+            return tcs.Task;
+        }
+    }
+}
