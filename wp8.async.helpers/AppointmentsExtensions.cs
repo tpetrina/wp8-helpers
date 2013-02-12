e7b8b356944c5dd850455b8d8ce7e4aed7f7fa73
Toni Petrina <toni.petrina@gmail.com>
2013-02-12 21:01:35 +0100

    Documentation and refactoring.

--- wp8.async.helpers/AppointmentsExtensions.cs
+++ wp8.async.helpers/AppointmentsExtensions.cs
@@ -6,78 +6,120 @@ namespace WP8.Async.Helpers
 {
     public static class AppointmentsExtensions
     {
-        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments @this, DateTime startTimeInclusive, DateTime endTimeInclusive, object state)
+        /// <summary>
+        /// Asynchronously searches for appointments that occur between the specified
+        /// start date and time and end date and time.
+        /// </summary>
+        /// <param name="appointments">The appointments which will be searched.</param>
+        /// <param name="startTimeInclusive">The start date and time to use to search for appointments.</param>
+        /// <param name="endTimeInclusive">The end date and time to use to search for appointments.</param>
+        /// <param name="state">A user-defined object that contains information about the operation.</param>
+        /// <returns>Search results.</returns>
+        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments appointments, DateTime startTimeInclusive, DateTime endTimeInclusive, object state)
         {
-            if (@this == null)
+            if (appointments == null)
                 throw new NullReferenceException();
 
             var tcs = new TaskCompletionSource<AppointmentsSearchEventArgs>();
             EventHandler<AppointmentsSearchEventArgs> handler = null;
             handler = (sender, e) =>
                 {
-                    @this.SearchCompleted -= handler;
+                    appointments.SearchCompleted -= handler;
                     tcs.TrySetResult(e);
                 };
 
-            @this.SearchCompleted += handler;
-            @this.SearchAsync(startTimeInclusive, endTimeInclusive, state);
+            appointments.SearchCompleted += handler;
+            appointments.SearchAsync(startTimeInclusive, endTimeInclusive, state);
 
             return tcs.Task;
         }
 
-        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments @this, DateTime startTimeInclusive, DateTime endTimeInclusive, Account account, object state)
+        /// <summary>
+        /// Asynchronously searches for appointments that occur between the specified
+        /// start date and time and end date and time, from the specified data source.
+        /// </summary>
+        /// <param name="appointments">The appointments which will be searched.</param>
+        /// <param name="startTimeInclusive">The start date and time to use to search for appointments.</param>
+        /// <param name="endTimeInclusive">The end date and time to use to search for appointments.</param>
+        /// <param name="account">The data source to search for appointments.</param>
+        /// <param name="state">A user-defined object that contains information about the operation.</param>
+        /// <returns>Search results.</returns>
+        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments appointments, DateTime startTimeInclusive, DateTime endTimeInclusive, Account account, object state)
         {
-            if (@this == null)
+            if (appointments == null)
                 throw new NullReferenceException();
 
             var tcs = new TaskCompletionSource<AppointmentsSearchEventArgs>();
             EventHandler<AppointmentsSearchEventArgs> handler = null;
             handler = (sender, e) =>
             {
-                @this.SearchCompleted -= handler;
+                appointments.SearchCompleted -= handler;
                 tcs.TrySetResult(e);
             };
 
-            @this.SearchCompleted += handler;
-            @this.SearchAsync(startTimeInclusive, endTimeInclusive, account, state);
+            appointments.SearchCompleted += handler;
+            appointments.SearchAsync(startTimeInclusive, endTimeInclusive, account, state);
 
             return tcs.Task;
         }
 
-        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments @this, DateTime startTimeInclusive, DateTime endTimeInclusive, int maximumItems, object state)
+        /// <summary>
+        /// Asynchronously searches for appointments that occur between the specified
+        /// start date and time and end date and time, returning no more than the specified
+        /// number of appointments.
+        /// </summary>
+        /// <param name="appointments">The appointments which will be searched.</param>
+        /// <param name="startTimeInclusive">The start date and time to use to search for appointments.</param>
+        /// <param name="endTimeInclusive">The end date and time to use to search for appointments.</param>
+        /// <param name="maximumItems">The maximum number of appointments to return.</param>
+        /// <param name="state">A user-defined object that contains information about the operation.</param>
+        /// <returns>Search results.</returns>
+        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments appointments, DateTime startTimeInclusive, DateTime endTimeInclusive, int maximumItems, object state)
         {
-            if (@this == null)
+            if (appointments == null)
                 throw new NullReferenceException();
 
             var tcs = new TaskCompletionSource<AppointmentsSearchEventArgs>();
             EventHandler<AppointmentsSearchEventArgs> handler = null;
             handler = (sender, e) =>
             {
-                @this.SearchCompleted -= handler;
+                appointments.SearchCompleted -= handler;
                 tcs.TrySetResult(e);
             };
 
-            @this.SearchCompleted += handler;
-            @this.SearchAsync(startTimeInclusive, endTimeInclusive, maximumItems, state);
+            appointments.SearchCompleted += handler;
+            appointments.SearchAsync(startTimeInclusive, endTimeInclusive, maximumItems, state);
 
             return tcs.Task;
         }
 
-        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments @this, DateTime startTimeInclusive, DateTime endTimeInclusive, int maximumItems, Account account, object state)
+        /// <summary>
+        /// Asynchronously searches for appointments that occur between the specified
+        /// start date and time and end date and time, from the specified data source,
+        /// returning no more than the specified number of appointments.
+        /// </summary>
+        /// <param name="appointments">The appointments which will be searched.</param>
+        /// <param name="startTimeInclusive">The start date and time to use to search for appointments.</param>
+        /// <param name="endTimeInclusive">The end date and time to use to search for appointments.</param>
+        /// <param name="maximumItems">The maximum number of appointments to return.</param>
+        /// <param name="account">The data source to search for appointments.</param>
+        /// <param name="state"></param>
+        /// <returns>Search results</returns>
+        public static Task<AppointmentsSearchEventArgs> SearchTaskAsync(this Appointments appointments, DateTime startTimeInclusive, DateTime endTimeInclusive, int maximumItems, Account account, object state)
         {
-            if (@this == null)
+            if (appointments == null)
                 throw new NullReferenceException();
 
             var tcs = new TaskCompletionSource<AppointmentsSearchEventArgs>();
             EventHandler<AppointmentsSearchEventArgs> handler = null;
             handler = (sender, e) =>
             {
-                @this.SearchCompleted -= handler;
+                appointments.SearchCompleted -= handler;
                 tcs.TrySetResult(e);
             };
 
-            @this.SearchCompleted += handler;
-            @this.SearchAsync(startTimeInclusive, endTimeInclusive, maximumItems, account, state);
+            appointments.SearchCompleted += handler;
+            appointments.SearchAsync(startTimeInclusive, endTimeInclusive, maximumItems, account, state);
 
             return tcs.Task;
         }
